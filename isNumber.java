631981f8657c2a26c3273f37e6f9059eb671c6f5
Ayush Vyas <ayushvyas7@gmail.com>
2017-12-05 14:30:17 -0800

    Changed isNumber implementation

--- isNumber.java
+++ isNumber.java
@@ -1,33 +1,27 @@
 public class isNumber {
-    public boolean isNumber(String s) {
-        boolean signOccured = false;
-        boolean dotOccured = false;
-        boolean started = false;
+
+    static String s = "-96.34";
+
+    public static void main(String[] args) {
+
+        System.out.print(check(s));
+    }
+
+    public static boolean check(String s) {
 
         s = s.trim();
-
+        int i = 0;
+        boolean dec = false; //is decimal?
         for (char c : s.toCharArray()) {
-            switch (c) {
-                case '+':
-                case '-':
-                    if (started || signOccured || dotOccured) {
-                        return false;
-                    } else {
-                        signOccured = true;
-                    }
-                    break;
-                case (char) ('0' - '9'):
-                    started = true;
-                    break;
-                case '.':
-                    if (dotOccured) {
-                        return false;
-                    } else {
-                        dotOccured = true;
-                    }
-                default:
-                    return false;
-            }
+            if (c == '-' || c == '+') {
+                if (i != 0) return false;
+            } else if (c == '.') {
+                if (dec)
+                    return false; //we can have only one decimal point in a number
+                else
+                    dec = true;
+            } else if (c < '0' || c > '9') return false;
+            i++;
         }
         return true;
     }
